4a0d3ec9fe2e21cecd9c1a0e1c60099d206915ee
mateusz <mateuszalmannai@googlemail.com>
2018-11-06 23:43:41 +0100

    extend roman numerals test

--- src/main/java/tudelft/roman/RomanNumeral.java
+++ src/main/java/tudelft/roman/RomanNumeral.java
@@ -5,33 +5,39 @@ import java.util.Map;
 
 public class RomanNumeral {
 
-    private static Map<Character, Integer> map;
+  private static Map<Character, Integer> map;
 
-    static {
-        map = new HashMap<Character, Integer>();
-        map.put('I', 1);
-        map.put('V', 5);
-        map.put('X', 10);
-        map.put('L', 50);
-        map.put('C', 100);
-        map.put('D', 500);
-        map.put('M', 1000);
+  static {
+    map = new HashMap<>();
+    map.put('I', 1);
+    map.put('V', 5);
+    map.put('X', 10);
+    map.put('L', 50);
+    map.put('C', 100);
+    map.put('D', 500);
+    map.put('M', 1000);
+  }
+
+  public int convert(String s) {
+
+    int convertedNumber = 0;
+    for (int i = 0; i < s.length(); i++) {
+      int currentNumber = map.get(s.charAt(i));
+      int next = i + 1 < s.length() ? map.get(s.charAt(i + 1)) : 0;
+      final boolean errorCondition01 = currentNumber == 5 && next == 10;
+      final boolean errorCondition02 = currentNumber == 10 && next == 100;
+
+      if (errorCondition01 || errorCondition02) {
+        throw new IllegalArgumentException("Invalid Roman Numeral");
+      }
+
+      if (currentNumber >= next)
+        convertedNumber += currentNumber;
+      else
+        convertedNumber -= currentNumber;
     }
 
-    public int convert(String s) {
+    return convertedNumber;
 
-        int convertedNumber = 0;
-        for(int i = 0; i < s.length(); i++) {
-            int currentNumber = map.get(s.charAt(i));
-            int next = i+1 < s.length() ? map.get(s.charAt(i+1)) : 0;
-
-            if(currentNumber >= next)
-                convertedNumber += currentNumber;
-            else
-                convertedNumber -= currentNumber;
-        }
-
-        return convertedNumber;
-
-    }
+  }
 }
--- src/test/java/tudelft/roman/RomanNumeralTest.java
+++ src/test/java/tudelft/roman/RomanNumeralTest.java
@@ -1,36 +1,54 @@
 package tudelft.roman;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
 public class RomanNumeralTest {
 
 
+    private RomanNumeral roman = new RomanNumeral();
+
     @Test
     public void singleNumber() {
-        RomanNumeral roman = new RomanNumeral();
-        int result = roman.convert("I");
-        Assertions.assertEquals(1, result);
+        assertEquals(1, roman.convert("I"));
+        assertEquals(5, roman.convert("V"));
+
     }
 
     @Test
     public void numberWithManyDigits() {
-        RomanNumeral roman = new RomanNumeral();
-        int result = roman.convert("VIII");
-        Assertions.assertEquals(8, result);
+        assertEquals(8, roman.convert("VIII"));
+        assertEquals(3, roman.convert("III"));
+        assertEquals(2, roman.convert("II"));
     }
 
     @Test
     public void numberWithSubtractiveNotation() {
-        RomanNumeral roman = new RomanNumeral();
-        int result = roman.convert("IV");
-        Assertions.assertEquals(4, result);
+        assertEquals(4, roman.convert("IV"));
+        assertEquals(9, roman.convert("IX"));
     }
 
     @Test
     public void numberWithAndWithoutSubtractiveNotation() {
-        RomanNumeral roman = new RomanNumeral();
-        int result = roman.convert("XLIV");
-        Assertions.assertEquals(44, result);
+        assertEquals(44, roman.convert("XLIV"));
+        assertEquals(19, roman.convert("XIX"));
+    }
+
+    @Test
+    void invalidNumber() {
+        validateRomanNumeral("VX");
+        validateRomanNumeral("XXC");
+    }
+
+    private void validateRomanNumeral(String romanNumeral) {
+        try {
+            roman.convert(romanNumeral);
+            fail("Expected Exception!");
+        } catch (IllegalArgumentException e) {
+            assertTrue(e.getMessage().contains("Invalid Roman Numeral"));
+        }
     }
 }
