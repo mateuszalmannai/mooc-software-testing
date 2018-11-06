212011dfca83013b66ebe3b56460b39da3142db5
mateusz <mateuszalmannai@googlemail.com>
2018-11-06 22:59:10 +0100

    fix NumFinder

--- src/main/java/tudelft/numfinder/NumFinderMain.java
+++ src/main/java/tudelft/numfinder/NumFinderMain.java
@@ -1,17 +1,29 @@
 package tudelft.numfinder;
 
+import java.util.Arrays;
+
 public class NumFinderMain {
 
-    public static void main (String[] args) {
-        NumFinder nf = new NumFinder();
+  public static void main(String[] args) {
 
-        // this works
-        // nf.find(new int[] {4, 25, 7, 9});
+    // this works
+    // nf.find(new int[] {4, 25, 7, 9});
 
-        // this crashes
-        nf.find(new int[] {4, 3, 2, 1});
+    doTheBusiness(new NumFinder(), new int[]{4, 3, 2, 1});
+    doTheBusiness(new NumFinder(), new int[]{104, 3333, 22, -1});
+    doTheBusiness(new NumFinder(), null);
+  }
 
-        System.out.println(nf.getLargest());
-        System.out.println(nf.getSmallest());
+  private static void doTheBusiness(NumFinder numFinder, int[] numberArray) {
+    numFinder.find(numberArray);
+
+    try {
+      System.out.println("Passed array: " + Arrays.toString(numberArray));
+      System.out.println("Largest int: " + numFinder.getLargest());
+      System.out.println("Smallest int: " + numFinder.getSmallest());
+      System.out.println("");
+    } catch (IllegalArgumentException e) {
+      System.out.println(e.getMessage());
     }
+  }
 }
