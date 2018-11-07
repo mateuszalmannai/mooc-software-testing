57670c53d49f973092bc14e0c3c49f153c897c7b
mateusz <mateuszalmannai@googlemail.com>
2018-11-07 17:55:56 +0100

    refactor roman numerals test

--- src/test/java/tudelft/roman/RomanNumeralTestWithBeforeEach.java
+++ src/test/java/tudelft/roman/RomanNumeralTestWithBeforeEach.java
@@ -1,8 +1,9 @@
 package tudelft.roman;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RomanNumeralTestWithBeforeEach {
 
@@ -10,30 +11,26 @@ public class RomanNumeralTestWithBeforeEach {
 
     @BeforeEach
     public void initialize() {
-        this.roman = new RomanNumeral();
+        roman = new RomanNumeral();
     }
 
     @Test
     public void singleNumber() {
-        int result = roman.convert("I");
-        Assertions.assertEquals(1, result);
+        assertEquals(1, roman.convert("I"));
     }
 
     @Test
     public void numberWithManyDigits() {
-        int result = roman.convert("VIII");
-        Assertions.assertEquals(8, result);
+        assertEquals(8, roman.convert("VIII"));
     }
 
     @Test
     public void numberWithSubtractiveNotation() {
-        int result = roman.convert("IV");
-        Assertions.assertEquals(4, result);
+        assertEquals(4, roman.convert("IV"));
     }
 
     @Test
     public void numberWithAndWithoutSubtractiveNotation() {
-        int result = roman.convert("XLIV");
-        Assertions.assertEquals(44, result);
+        assertEquals(44, roman.convert("XLIV"));
     }
 }
